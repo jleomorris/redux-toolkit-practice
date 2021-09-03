3ed4749f40e45d1f8d6bb3b7ba951640b785456b
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-03 19:48:48 +0100

    Update content and design of homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        home_bg:
+          'url(https://res.cloudinary.com/jleomorris/image/upload/f_auto,q_auto/e_gradient_fade,x_0.5,b_black/v1630675027/redux-counter-quote-generator/home.jpg)',
+        chuck:
+          'url(https://res.cloudinary.com/jleomorris/image/upload/f_auto,q_auto/e_gradient_fade,x_0.8,b_black/v1630661079/redux-counter-quote-generator/quote-generator/main-background.jpg)',
+      }),
+    },
   },
   variants: {
     extend: {},
